32d77ce1c0332a74c603115bab432dcbbcaa3b3f
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-02-17 10:54:32 +0530

    Time: 8 ms (74.76%), Space: 10.9 MB (72.35%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void solve(int idx,vector<int>& candidates, int target, vector<int>&res){
+        if(target==0){
+            ans.push_back(res);
+            return ;
+        }
+        if(idx<0) 
+            return;
+        if(target>=candidates[idx]){
+            res.push_back(candidates[idx]);
+            solve(idx,candidates,target-candidates[idx],res);
+            res.pop_back();
+        }
+        solve(idx-1,candidates,target,res);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n=candidates.size();
+        ans.clear();
+        vector<int>res;
+        solve(n-1,candidates,target,res);
+        return ans;
+    }
+};
