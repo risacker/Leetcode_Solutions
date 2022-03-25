637691e2dc2db73acaef245fe8e3910062289c8b
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-03-25 22:03:11 +0530

    Time: 791 ms (9.60%), Space: 10.3 MB (66.95%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,22 +1,21 @@
 class Solution {
  public:
       vector<int> twoSum(vector<int>& nums, int target) {
-      int sum=0;
-      bool got=false;
+      int sum = 0;
+      bool got = false;
       vector<int> ans;
         for(int i = 0; i < nums.size(); i++){
-             for(int j = i+1; j < nums.size(); j++)
-             {
-                 sum=nums[i]+nums[j];
+             for(int j = i+1; j < nums.size(); j++){
+                 sum = nums[i]+nums[j];
                      if(sum == target){
                        ans.push_back(i);
                        ans.push_back(j);
                        got = true;
                        break;
-              }
-          if(got == true)
-              break;
-         }
+                     }
+                 if(got == true)
+                    break;
+             }
         }
         return ans;
       }
