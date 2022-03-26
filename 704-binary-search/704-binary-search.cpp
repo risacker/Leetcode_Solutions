8cac32f55a0166ff466fe9aa76a63467f7461c68
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-03-26 18:36:33 +0530

    Time: 57 ms (38.64%), Space: 27.5 MB (92.40%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    int binary_search(vector<int> &nums, int target, int si, int ei)
+    {
+        if(si >= ei && nums[si] != target)
+            return -1;        
+        int mid = (si + ei) / 2;
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] > target)
+            return binary_search(nums, target, si, mid - 1);
+        else
+            return binary_search(nums, target, mid + 1, ei);
+    }
+   public:
+    int search(vector<int>& nums, int target) {
+        return binary_search(nums, target, 0, nums.size() - 1);
+    }
+};
