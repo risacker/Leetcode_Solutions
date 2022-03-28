ff333331941279a45431eaebd22362bc6aced632
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-03-28 18:33:21 +0530

    Time: 332 ms (17.61%), Space: 68.9 MB (18.67%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> trust_judge(n+1, 0);
+        
+        for (auto t : trust) {
+            trust_judge[t[0]]--;
+            trust_judge[t[1]]++;
+        }
+        
+        for (int i = 1; i <= n; i++)
+            if (trust_judge[i] == n-1) return i;
+        
+        return -1;
+    }
+};
