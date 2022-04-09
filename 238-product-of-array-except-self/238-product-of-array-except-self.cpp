0d9660fa01a8e8e46d023260d871a3d2e708460c
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-04-09 18:14:14 +0530

    Time: 24 ms (85.02%), Space: 23.8 MB (93.01%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -5,7 +5,7 @@ public:
 	    int r;
 	    int lprod = 1;
 	    int rprod = 1;
-	    for(int l=0; l<nums.size(); l++) {
+	    for(int l = 0; l < nums.size(); l++) {
             r = nums.size() - l - 1;
 		    products[l] *= lprod;
 		    products[r] *= rprod;
