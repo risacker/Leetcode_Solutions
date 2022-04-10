3f64b82487c2ee0ce23e2d6526283403a99d7ed7
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-04-10 18:42:51 +0530

    Time: 7 ms (52.20%), Space: 12.1 MB (37.47%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size(), k, l;
-    	for (k = n - 2; k >= 0; k--) {
-            if (nums[k] < nums[k + 1]) {
+    	for (k = n - 2; k >= 0; k--) {// finding breakpoint which we know the 1st breakpoint can be at second last index
+            if (nums[k] < nums[k + 1]) {// linearly iterate from 2nd last index and try to find breakpoint that is k and if we find the breakpoint then break out.
                 break;
             }
         }
-    	if (k < 0) {
+    	if (k < 0) {// if there's no breakpoint then simply reverse it
     	    reverse(nums.begin(), nums.end());
     	} 
-        else {
+        else {// if there's a breakpoint then again we linearly iterate from back
     	    for (l = n - 1; l > k; l--) {
-                if (nums[l] > nums[k]) {
+                if (nums[l] > nums[k]) {// if any element is greater then break out
                     break;
                 }
             } 
-    	    swap(nums[k], nums[l]);
-    	    reverse(nums.begin() + k + 1, nums.end());
+    	    swap(nums[k], nums[l]);// swap it
+    	    reverse(nums.begin() + k + 1, nums.end());//everything on right will be reversed using inbuilt stl 
         }
     }
 };
