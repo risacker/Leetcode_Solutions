fa1c149239268129220721eeb69681b3ec74a8d8
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-04-11 19:12:45 +0530

    Time: 46 ms (35.53%), Space: 25.1 MB (52.72%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
      void rotate(vector<int>& nums, int k) {
-        if(nums.size()!=1){
-            k = k%nums.size();
-        vector<int> tmp(nums.size());
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i+k <= nums.size()-1)
-            {
-                tmp[i+k] = nums[i];
-            }
-            else
-            {
-                int dif = i+k-(nums.size()-1)-1;
-                tmp[dif] = nums[i];
-            }
-        }
-        nums = tmp;
-      }
+        int n = nums.size();
+        int j = k%n;
+        vector a(nums.begin(), nums.begin()+n-j );
+        nums.erase(nums.begin(), nums.end()-j);
+        nums.insert(nums.end(),a.begin(),a.end()); 
     }
 };
