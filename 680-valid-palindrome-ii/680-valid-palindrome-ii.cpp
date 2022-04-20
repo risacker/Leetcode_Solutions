8eef8cb9f80ac0044c4b6ecadc3f9d14cf7fe4c2
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-04-20 17:49:46 +0530

    Time: 75 ms (38.71%), Space: 24 MB (28.76%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool checkPalindrome(string str,int start,int end){
-        while(start < end)
-        {
+        while(start < end){
             if(str[start] != str[end])
                 return false;
             start++;
@@ -10,8 +9,9 @@ public:
         }
         return true;
      }
+    
      bool validPalindrome(string s) {
-        int start = 0,end = s.size()-1;
+        int start = 0, end = s.size()-1;
         while(start < end){
             if(s[start] != s[end])
              return(checkPalindrome(s,start+1,end) || checkPalindrome(s,start,end-1));
