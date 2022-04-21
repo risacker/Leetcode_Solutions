7ad0a96aa2c4822623a958deb4d808688da4b641
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-04-21 14:58:46 +0530

    Time: 590 ms (9.32%), Space: 40.1 MB (90.84%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -4,45 +4,44 @@ public:
      MyHashSet() {
 	   // creating a linked list
         head = new ListNode(-1);
-    }
+     }
     
-    void add(int key) {
+     void add(int key) {
         ListNode *temp = head;
         while(temp->next != NULL) 
         {  
             // condtion for duplicates
             if(temp->next->val == key) 
                 return ;		
-            temp=temp->next;
+            temp = temp->next;
         }     
         ListNode* newnode = new ListNode(key);
         temp->next = newnode;
-    }
+     }
     
-    void remove(int key) {
+     void remove(int key) {
         ListNode *temp = head;    
-        while( temp->next != NULL) 
+        while(temp->next != NULL) 
         {   
             if(temp->next->val == key)
             {
               temp->next = temp->next->next;
               break;
              }   
-         
-            temp=temp->next;
+            temp = temp->next;
         }
-    }
+     }
     
-    bool contains(int key) { 
+     bool contains(int key) { 
         ListNode *temp = head->next; 
         while(temp != NULL)
         {   
             if(temp->val == key) 
                 return true;
-            temp=temp->next;
+            temp = temp->next;
         }
         return false;
-    }
+     }
 };
 
 /**
