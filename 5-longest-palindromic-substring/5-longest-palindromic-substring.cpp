f651e376ac6f6dfa3e2a20ca501ba9f793fa162e
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-04-21 15:44:19 +0530

    Time: 42 ms (69.62%), Space: 6.9 MB (97.95%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    string longestPalindrome(string A) {
-       int N = A.size();   
-        if(N == 0)
-            return "";
-        if(N == 1)
-            return A ;   
-       bool t[N][N];
-       memset(t, 0, sizeof t);
-       for(int i = 0;i < N;i++)
-           t[i][i] = 1 ;    
-        int mx = 1 ; 
-        int in = 0 ;     
-       for(int L = 2;L <= N ;L++ )
-       for(int i = 0 ;i+L-1 < N; i++){
-           int j = i+L-1;
-           t[i][j] = A[i] == A[j] && (L == 2 || t[i+1][j-1]) ;     
-           if(t[i][j]){
-               if(L > mx){
-                   mx = L ; 
-                   in = i ; 
-               }    
-           }
-       }
-       return A.substr(in , mx);  
+    string longestPalindrome(string s) {
+        int maxLen = 1,start = 0;
+        int l,h;
+        int n = s.length();
+        for(int i = 0;i < n;i++){
+            l = i-1;
+            h = i+1;
+            while(h < n && s[i] == s[h])
+                h++;
+            while(l >= 0 && s[i] == s[l])
+                l--;
+            while(l >= 0 && h < n && s[l] == s[h]){
+                h++;
+                l--;
+            }
+            int len = h-l-1;
+            if(len > maxLen){
+                maxLen = len;
+                start = l+1;
+            }
+        }
+        return s.substr(start, maxLen);
     }
 };
