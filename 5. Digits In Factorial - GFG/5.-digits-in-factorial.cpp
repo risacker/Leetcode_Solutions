12d56ec5638b40c292fe5aee43483bdc52b62c1a
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-05-18 23:46:36 +0530

    Added solution - LeetHub

---
+++ 5. Digits In Factorial - GFG/5.-digits-in-factorial.cpp
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int digitsInFactorial(int N)
+    {
+        // code here
+        double sum = 0;
+        for(int i = 1; i <= N; i++)
+            sum += log10(i);
+        return floor(sum) + 1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int T;
+    
+    //taking testcases
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        
+        //taking N
+        cin>>N;
+        Solution ob;
+        //calling method digitsInFactorial()
+        cout<<ob.digitsInFactorial(N)<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
