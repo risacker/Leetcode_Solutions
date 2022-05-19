146382b840c048a8dbaa201591ad6bccda588eb0
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-05-19 23:30:55 +0530

    Added solution - LeetHub

---
+++ 9. Addition Under Modulo - GFG/9.-addition-under-modulo.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    long long sumUnderModulo(long long a,long long b){
+        // code here
+        long c = a%1000000007;
+        long d = b%1000000007;
+        return (c+d)%1000000007;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    long long a;
+	    long long b;
+	    cin>>a>>b;
+	    Solution ob;
+	    cout<<ob.sumUnderModulo(a,b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
