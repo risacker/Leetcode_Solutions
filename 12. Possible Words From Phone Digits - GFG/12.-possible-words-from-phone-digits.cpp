202477a07c3eb9732cefb8c13d05681ad9b5d505
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-05-27 23:53:36 +0530

    Added solution - LeetHub

---
+++ 12. Possible Words From Phone Digits - GFG/12.-possible-words-from-phone-digits.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find list of all words possible by pressing given numbers.
+    vector<string> possibleWords(int a[], int N)
+    {
+        //Your code here
+        vector<string> m{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans{""};
+	    for(int i = 0; i < N; i++){
+	    	vector<string> e;
+		    for(auto& c : ans)
+			    for(auto n : m[a[i]-2])
+				    e.push_back(c + n);             
+		        ans = move(e); 
+	    }
+	    return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+  
+	int T;
+	
+	cin >> T; //testcases
+	
+	while(T--){ //while testcases exist
+	   int N;
+	    
+	   cin >> N; //input size of array
+	   
+	   int a[N]; //declare the array
+	   
+	   for(int i =0;i<N;i++){
+	       cin >> a[i]; //input the elements of array that are keys to be pressed
+	   }
+	   
+	   Solution obj;
+	   
+	  vector <string> res = obj.possibleWords(a,N);
+	  for (string i : res) cout << i << " ";
+	   cout << endl;
+	}
+	
+	return 0;
+}  // } Driver Code Ends
