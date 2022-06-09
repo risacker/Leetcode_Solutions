d7356515dceb730783ad0108e9368131bccee9ed
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-06-09 13:28:27 +0530

    Time: 0 ms, Memory: 5.8 MB - LeetHub

---
+++ happy-number/happy-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int newNumber = 0;
+        if(n == 1 || n== 7) 
+            return true;
+        else if(n < 10) 
+            return false;
+        while(n != 0) {
+            newNumber += pow(n%10,2);
+            n = n/10;
+        }
+        return isHappy(newNumber);
+    }
+};
