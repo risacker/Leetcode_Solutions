8267af6b6fbca5bc6e895c3a5399775a56800a82
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-06-10 13:19:11 +0530

    Time: 159 ms (74.92%), Space: 208.1 MB (38.47%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,80 +1,21 @@
-class Node{
-public:
-    int key;
-    int value;  
-    Node* next;
-    Node()
-    {
-        next = 0;
-    }
-    Node(int key,int value)
-    {
-        this->key = key;
-        this->value  = value;
-        next = 0;
-    }
-};
-
 class MyHashMap {
-    Node* head;
+   
+private:
+    vector<int> v;
+    
 public:
-    MyHashMap() {
-        head = 0;
+    MyHashMap() : v(1e6+1, -1) {}
+    
+    void put(int key, int value){
+        v[key] = value;
     }
     
-    void put(int key, int value) 
-    {
-        Node* temp = head,*last{head};
-        int flag=0;   
-        if(!head)
-        {
-            head = new Node(key,value);
-            return ;
-        }
-        while(temp)
-        {
-            if(temp->key == key)
-            {
-                temp->value = value;
-                flag = 1;
-            }
-            last= temp;
-            temp= temp->next;
-        }    
-        if(flag==0)
-        {
-            last->next = new Node(key,value);    
-        }
-    } 
-    int get(int key) {   
-        Node* temp = head;
-        int flag = 0;     
-        while(temp)
-        {
-            if(temp->key == key)
-            {
-                return temp->value;
-            }
-            temp = temp->next;   
-        }
-        return -1;
+    int get(int key){
+        return v[key];
     }
-    void remove(int key) {
-        Node* temp = head,*last{head};   
-        while(temp)
-        {
-            if(temp->key == key)
-            {
-                if(temp == head)
-                {
-                    head = head->next;
-                    return ;
-                } 
-                last->next = temp->next;
-            }
-            last = temp;
-            temp = temp->next;
-        }
+    
+    void remove(int key){
+        v[key] = -1;
     }
 };
 
