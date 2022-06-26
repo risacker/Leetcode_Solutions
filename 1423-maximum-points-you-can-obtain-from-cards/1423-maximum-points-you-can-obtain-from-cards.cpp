89fe08c8c15c9f0329a8634c7a8acb35077c077a
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-06-26 08:01:54 +0530

    Time: 117 ms (19.36%), Space: 42.4 MB (80.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -5,17 +5,15 @@ public:
         long long sum = 0;
         for(long long i = 0;i < k;i++)
             sum += cardPoints[i]; 
-        long long maxi = sum;   
+        long long maxm = sum;   
         long long c = n;
         long long d = k-1; 
         while(c-- > n - k){
             if(d < 0 )
                 d = n-1;
             sum = sum - cardPoints[d--] + cardPoints[c];
-            maxi = max(sum, maxi);
-           
+            maxm = max(sum, maxm);   
         }
-        return maxi;
-        
+        return maxm;     
     }
 };
