9abbfc75fa25edf15c6cbb8ccb32539dc17f33e3
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-07-09 19:55:48 +0530

    Time: 235 ms (78.07%), Space: 81.9 MB (61.68%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> dp(nums.size());
+        priority_queue<int> pq;
+        dp[n-1] = nums[n-1];
+        deque<int> dq;
+        dq.push_back(n-1); 
+        for(int i = n-2;i >= 0;i--){
+            int a = nums[i];
+            while(dq.size() && dq.front() > i+k){
+                dq.pop_front();
+            }
+            int next = dp[dq.front()];
+            dp[i] = a+next;
+          while(dq.size() && dp[dq.back()] < dp[i]){
+              dq.pop_back();
+          }  
+            dq.push_back(i); 
+        }
+        return dp[0];
+    }
+};
