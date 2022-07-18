328534bf28b753051b6ab65a2850c448f5718241
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-07-18 13:37:37 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (70.35%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -3,11 +3,11 @@ public:
     int minimumSubarraysRequired(vector<int>& nums, int maxSumAllowed) {
         int currentSum = 0;
         int splitsRequired = 0;
-        for (int element : nums) {
-            if (currentSum + element <= maxSumAllowed) {
+        for(int element : nums){
+            if(currentSum + element <= maxSumAllowed){
                 currentSum += element;
             }
-            else {
+            else{
                 currentSum = element;
                 splitsRequired++;
             }
@@ -18,7 +18,7 @@ public:
     int splitArray(vector<int>& nums, int m) {
         int sum = 0;
         int maxElement = INT_MIN;
-        for (int element : nums) {
+        for(int element : nums){
             sum += element;
             maxElement = max(maxElement, element);
         }
@@ -26,15 +26,14 @@ public:
         int left = maxElement;
         int right = sum;
         int minimumLargestSplitSum = 0;
-        while (left <= right) {
+        while(left <= right){
             int maxSumAllowed = left + (right - left) / 2;
-            if (minimumSubarraysRequired(nums, maxSumAllowed) <= m) {
+            if(minimumSubarraysRequired(nums, maxSumAllowed) <= m){
                 right = maxSumAllowed - 1;
                 minimumLargestSplitSum = maxSumAllowed;
             } 
-            else {
+            else 
                 left = maxSumAllowed + 1;
-            }
         }      
         return minimumLargestSplitSum;
     }
