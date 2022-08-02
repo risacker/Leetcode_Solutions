6e103b31dd6208118f5391e3fef56d456ec34ccb
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-08-02 09:31:14 +0530

    Time: 60 ms (39.69%), Space: 14.6 MB (16.78%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -4,8 +4,7 @@ public:
         priority_queue <int, vector<int>, greater<int> > pq;
         int n = matrix.size();
         for(int i = 0;i < n;i++){
-            for(int j = 0;j < n;j++)
-            {
+            for(int j = 0;j < n;j++){
                 pq.push(matrix[i][j]);
             }
         }
