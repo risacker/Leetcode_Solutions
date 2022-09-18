12416126d0e83f1b8d2d31b1255e0af9060e7d22
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2022-09-18 11:04:15 +0530

    Time: 32 ms (32.63%), Space: 20.6 MB (25.17%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -7,15 +7,14 @@ public:
         mxL[0] = height[0];
         for(int i = 1;i < n;i++)
             mxL[i] = max(mxL[i-1],height[i]);    
-        mxR[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--)
-            mxR[i]=max(mxR[i+1],height[i]);    
+        mxR[n-1] = height[n-1];
+        for(int i = n-2;i >= 0;i--)
+            mxR[i] = max(mxR[i+1], height[i]);    
         int water[n];
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            water[i]=min(mxL[i],mxR[i])-height[i];
-            sum=sum+water[i];
+        int sum = 0;
+        for(int i = 0;i < n;i++){
+            water[i] = min(mxL[i],mxR[i])-height[i];
+            sum = sum+water[i];
         }
         return sum;
     }
