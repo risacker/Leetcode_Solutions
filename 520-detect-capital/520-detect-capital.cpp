80774f5a5a395a036d4c8db20ac0091d21496b8d
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2023-01-02 11:39:02 +0530

    Time: 2 ms (56.84%), Space: 6.1 MB (24.57%) - LeetHub

--- 520-detect-capital/520-detect-capital.cpp
+++ 520-detect-capital/520-detect-capital.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
      bool detectCapitalUse(string word) {
-      int count1=0,count2=0;
-        for(char c:word)
-        {
-            if(c>=97&&c<=122)
+      int count1 = 0, count2 = 0;
+        for(char c : word){
+            if(c >= 97 && c <= 122)
               count1++;
-            else if(c<=90&&c>=65)
+            else if(c <= 90&&c >= 65)
                 count2++;
         }
-        if(word[0]<=90&&word[0]>=65)
-          return (count2==1)||(count2==word.size());
-        return count1==word.size();
+        if(word[0] <= 90 && word[0] >= 65)
+          return (count2 == 1)||(count2 == word.size());
+        return count1 == word.size();
     }
 };
