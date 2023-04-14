c0d6acad39040cc48319989a0cf1080fbd39a3ec
RISHIKA SWARAJ <79155106+risacker@users.noreply.github.com>
2023-04-14 18:46:04 +0530

    Time: 15 ms (50.04%), Space: 13.3 MB (41.69%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size()+1, 0);
+        dp[0] = true;
+        unordered_set<string> set(wordDict.begin(), wordDict.end());
+        for(int i = 0; i <= s.size(); i++){
+            for(int j = 0; j <= i; j++){
+                if(dp[j] && set.count(s.substr(j, i-j))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
